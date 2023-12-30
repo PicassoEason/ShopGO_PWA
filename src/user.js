e254f3197d4acfd91410765a6c53ac565b3f1f34
Picasso <87004138+PicassoEason@users.noreply.github.com>
2023-12-30 23:27:27 +0800

    logout

--- src/user.js
+++ src/user.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import {auth} from './utils/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useNavigate  } from 'react-router-dom';
+import { useEffect, useState } from "react"
 import Footer from './component/footer';
 import Goog from './img/google.png';
 export default function App() {
+
+    const [token ,setToken] = useState('')
+    const [user,loading]=useAuthState(auth)
+    const [username, setUsername] = useState('');
+    console.log(token)
+    const navigate = useNavigate();
+    useEffect(() => {
+      setToken(localStorage.getItem('token'))
+      if (user) {
+        setUsername(user.displayName);
+      }
+    },[user])
+    if (loading) return <h1>Loading ....</h1>
+    
+    const logout=async()=>{
+        auth.signOut();  // 登出 Firebase 認證
+        localStorage.removeItem('token');
+        if ((!user)|| (token=="")) 
+        navigate('/');  // 使用 navigate 進行導向
+        
+    }
+    if (user || token !="")
     return(<>
         <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
             <div style={{left: 119, top: 80, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>歡 迎 回 來</div>
             <div style={{width: 300, height: 65, left: 59, top: 559, position: 'absolute', background: '#64A2FF', borderRadius: 5, border: '3px black solid'}} />
-             
+
             <>
             {/* 登出按鈕 */}
             <button 
@@ -23,16 +49,13 @@ export default function App() {
                 background: 'none',  // 移除背景色
                 cursor: 'pointer'  // 添加手形游標
               }}
-              onClick={() => {
-                // 處理登出的邏輯
-                console.log("登出");
-              }}
+              onClick={logout}
             >
               登出
             </button>
             </>
 
-            <div style={{left: 128, top: 155, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Ezra WU</div>
+            <div style={{left: 128, top: 155, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>{username}</div>
             <img style={{width: 300, height: 195, left: 59, top: 227, position: 'absolute'}} src={Goog} />
         </div>
         <Footer></Footer>
