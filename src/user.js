48868b6b6783af78358235f240edf9e4d30ab204
Picasso <87004138+PicassoEason@users.noreply.github.com>
2023-12-30 23:09:43 +0800

    delate 我需要幫助 button

--- src/user.js
+++ src/user.js
@@ -6,7 +6,7 @@ export default function App() {
         <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
             <div style={{left: 119, top: 80, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>歡 迎 回 來</div>
             <div style={{width: 300, height: 65, left: 59, top: 559, position: 'absolute', background: '#64A2FF', borderRadius: 5, border: '3px black solid'}} />
-            <div style={{width: 300, height: 65, left: 59, top: 458, position: 'absolute', background: '#FF6D64', borderRadius: 5, border: '3px black solid'}} />
+             
             <>
             {/* 登出按鈕 */}
             <button 
@@ -29,29 +29,6 @@ export default function App() {
               }}
             >
               登出
-            </button>
-          
-            {/* 我需要幫助按鈕 */}
-            <button 
-              style={{
-                left: 129, 
-                top: 471, 
-                position: 'absolute', 
-                color: 'black', 
-                fontSize: 32, 
-                fontFamily: 'Inter', 
-                fontWeight: '650', 
-                wordWrap: 'break-word',
-                border: 'none',  // 移除邊框
-                background: 'none',  // 移除背景色
-                cursor: 'pointer'  // 添加手形游標
-              }}
-              onClick={() => {
-                // 處理"我需要幫助"的邏輯
-                console.log("我需要幫助");
-              }}
-            >
-              我需要幫助
             </button>
             </>
 
