adb492c530ddd844445dd68ca4682018086b5879
Picasso <87004138+PicassoEason@users.noreply.github.com>
2023-12-30 19:36:33 +0800

    調整首頁畫面大小

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import Button from '@mui/joy/Button';
+import care from './img/index.png';
 
 function App() {
   return (
-    <>
-   
-    </>
+    <div className="app-container">
+      <div className="content">
+        <img className="care-image" src={care} alt="Care Connect Logo" />
+        <div className="title">Welcome to</div>
+        <div className="subtitle">CareConnect!</div>
+        <div className="description">CareConnect is your best friend for all person.</div>
+        <div className="button-container">
+          <Button>使用 Google 登入</Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
